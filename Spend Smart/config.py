7ffa1f4b806a38a49ef78fd705e0752b38a45b57
Anurag-Jaswal <100454718+Anurag-Jaswal@users.noreply.github.com>
2025-06-01 23:21:02 +0530

    Update config.py

--- Spend Smart/config.py
+++ Spend Smart/config.py
@@ -2,7 +2,7 @@ class Config:
     MYSQL_HOST = "localhost"
     MYSQL_USER = "root"
     MYSQL_PASSWORD = "33510jaswal"  # Your MySQL password
-    MYSQL_DB = "wealthyinfyme"
+    MYSQL_DB = "wealthyinfyme2"
     MYSQL_PORT = 3306  # Default MySQL port
     MYSQL_UNIX_SOCKET = '/path/to/your/mysql/socket'  # Only needed for Unix socket connections
     SECRET_KEY = "This_is_a_super_secret_key"
